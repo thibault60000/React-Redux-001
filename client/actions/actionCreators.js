38a104e219e064b57b2e6f32cfeb47f60e919521
Thibault JP <thibault60000@gmail.com>
2018-04-15 17:27:07 +0200

    add firsts actions

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -1,9 +1,28 @@
 // Increment
-
+function increment(index) {
+    return {
+        type: 'INCREMENT_LIKES',
+        index,
+    }
+}
 
 
 // Add comment
-
+function addComment(postId, author, comment) {
+    return {
+        type: 'ADD_COMMENT',
+        postId,
+        author,
+        comment
+    }
+}
 
 
 // Remove Comment
+function removeComment(postId, i) {
+    return {
+        type: 'REMOVE_COMMENT',
+        i,
+        postId
+    }
+}
